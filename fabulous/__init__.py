3a6d07114a6f0c28ba5d2e6c42b9411ba8a5f8cd
Paul Walsh <paulywalsh@gmail.com>
2013-11-13 09:47:20 +0200

    Fixing for setting the execution environment

--- fabulous/__init__.py
+++ fabulous/__init__.py
@@ -9,23 +9,15 @@ from fabulous import remote
 
 
 @task
-def e(environment=None):
+def e(environment='local'):
     utilities.notify(u'Setting the environment for this task run.')
 
-    # if no environment name passed, we work on local
-    activated_environment = u'LOCAL'
+    project_config = importlib.import_module('fabfile.config')
+    project_sensitive = importlib.import_module('fabfile.sensitive')
+    env_config = getattr(project_config, environment.upper())
+    env_sensitive = getattr(project_sensitive, environment.upper() + '_SENSITIVE')
+    env.update(env_config)
+    env.update(env_sensitive)
+    env.roles = [environment]
 
-    if environment:
-        # TODO: We let this fail if anything here is not found. Handle in a more friendly fashion.
-        project_config = importlib.import_module('fabfile.config')
-        project_sensitive = importlib.import_module('fabfile.sensitive')
-        env_config = getattr(project_config, environment.upper())
-        env_sensitive = getattr(project_sensitive, environment.upper() + '_SENSITIVE')
-        env.update(env_config)
-        env.update(env_sensitive)
-
-        env.roles = [environment]
-
-        activated_environment = unicode(environment.upper())
-
-    utilities.notify(u'The execution environment is ' + activated_environment)
+    utilities.notify(u'The execution environment is ' + unicode(environment.upper()))
