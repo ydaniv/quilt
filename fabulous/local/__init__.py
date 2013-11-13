f777ee0c9d6eff955b278e9a2a751ccd4a405988
Paul Walsh <paulywalsh@gmail.com>
2013-11-13 09:10:12 +0200

    update all local init tasks

--- fabulous/local/__init__.py
+++ fabulous/local/__init__.py
@@ -1,78 +1,9 @@
-import logging
-import importlib
-from fabric.api import env, prefix, task, roles, run
+from fabric.api import env, task, local
 from fabulous import config, utilities
 
 env.update(config.FABULOUS_DEFAULT)
 
 from . import cache, db, environ, server
-
-
-@task
-def e(environment=None):
-    utilities.notify(u'Setting the environment for this task run.')
-
-    # if no environment name passed, we work on local
-    activated_environment = u'LOCAL'
-
-    if environment:
-        # TODO: We let this fail if anything here is not found. Handle in a more friendly fashion.
-        project_config = importlib.import_module('fabfile.config')
-        project_sensitive = importlib.import_module('fabfile.sensitive')
-        env_config = getattr(project_config, environment.upper())
-        env_sensitive = getattr(project_sensitive, environment.upper() + '_SENSITIVE')
-        env.update(env_config)
-        env.update(env_sensitive)
-
-        env.roles = [environment]
-
-        activated_environment = unicode(environment.upper())
-
-    utilities.notify(u'The execution environment is ' + activated_environment)
-
-
-@task
-def build_remote():
-    utilities.notify(u'Now starting the project bootstrap sequence.')
-
-    environ.make()
-    clone()
-    environ.ensure()
-    environ.settings()
-    validate()
-    migrate()
-    collectstatic()
-    server.nginx()
-    server.gunicorn()
-    #server.celery()
-
-
-@task
-def upgrade():
-    utilities.notify(u'Now starting the project upgrade sequence.')
-
-    fetch()
-    merge()
-    environ.ensure()
-    environ.settings()
-    validate()
-    migrate()
-    collectstatic()
-    server.nginx()
-    server.gunicorn()
-    #server.celery()
-
-
-@task
-def deploy():
-    utilities.notify(u'Now starting the project deploy sequence.')
-
-    fetch()
-    merge()
-    validate()
-    migrate()
-    collectstatic()
-    server.restart()
 
 
 @task
@@ -94,57 +25,56 @@ def bootstrap(initial='no', environment='no', clear_cache='no'):
 
 
 @task
+def upgrade():
+    utilities.notify(u'Now starting the project upgrade sequence.')
+
+    fetch()
+    merge()
+    environ.ensure()
+    validate()
+    migrate()
+
+
+@task
 def clone():
     utilities.notify(u'Now cloning from the remote repository.')
 
-    with prefix(env.workon):
-        run('git clone ' + env.repository_location + ' .')
-        run(env.deactivate)
+    local('git clone ' + env.repository_location + ' .')
 
 
 @task
 def fetch():
     utilities.notify(u'Now fetching from the remote repository.')
 
-    with prefix(env.workon):
-        run('git fetch')
-        run(env.deactivate)
+    local('git fetch')
 
 
 @task
 def merge():
     utilities.notify(u'Now merging from the remote repository.')
 
-    with prefix(env.workon):
-        run('git merge ' + env.repository_work_branch + ' origin/' + env.repository_work_branch)
-        run(env.deactivate)
+    local('git merge ' + env.repository_work_branch + ' origin/' + env.repository_work_branch)
 
 
 @task
 def validate():
     utilities.notify(u'Now running Django validations.')
 
-    with prefix(env.workon):
-        run('python manage.py validate')
-        run(env.deactivate)
+    local('python manage.py validate')
 
 
 @task
 def migrate():
     utilities.notify(u'Now running Django migrations.')
 
-    with prefix(env.workon):
-        run('python manage.py syncdb --noinput --migrate')
-        run(env.deactivate)
+    local('python manage.py syncdb --noinput --migrate')
 
 
 @task
 def collectstatic():
     utilities.notify(u'Now running Django static asset collector.')
 
-    with prefix(env.workon):
-        run('python manage.py collectstatic')
-        run(env.deactivate)
+    local('python manage.py collectstatic')
 
 
 @task
@@ -172,6 +102,4 @@ def sanity():
 def command(cmd):
     utilities.notify(u'Now executing the command you passed.')
 
-    with prefix(env.workon):
-        run(cmd)
-        run(env.deactivate)
+    local(cmd)
