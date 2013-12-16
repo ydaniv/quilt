0a9f399e5fda6c6db9e76abc6c195f0385f52ec9
Paul Walsh <paulywalsh@gmail.com>
2013-12-16 21:17:21 +0200

    fix import files from the project

--- quilt/__init__.py
+++ quilt/__init__.py
@@ -1,19 +1,19 @@
 from fabric.api import env, task
 from quilt import local, remote, contrib, config, utilities
-from fabfile import quilt
+from fabfile import quilt_config
 
 
 @task
 def e(environment='local'):
     utilities.notify(u'Setting the environment for this task run.')
 
-    env_config = getattr(quilt, environment.upper())
+    env_config = getattr(quilt_config, environment.upper())
     env.update(env_config)
     env.roles = [environment]
 
     try:
-        from fabfile import sensitive
-        env_sensitive = getattr(sensitive, environment.upper())
+        from fabfile import quilt_sensitive
+        env_sensitive = getattr(quilt_sensitive, environment.upper())
         env.update(env_sensitive)
     except ImportError:
         pass
