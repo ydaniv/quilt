7c9223d4a438b469c829180b7183ab324e3c250e
Paul Walsh <paulywalsh@gmail.com>
2014-02-27 08:48:02 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ with open('README.rst') as f:
 
 setup(
     name='Quilt',
-    version='0.1-alpha',
-    description='Quilt is an opinionated set of Fabric tasks for local '
+    version='0.1.1-alpha',
+    description='(WIP) Quilt is an opinionated set of Fabric tasks for local '
                 'development and remote deployment of web apps.',
     long_description=long_description,
     url='https://github.com/pwalsh/quilt',
@@ -16,9 +16,6 @@ setup(
     author_email='paulywalsh@gmail.com',
     license='BSD',
     packages=['quilt', 'quilt.local', 'quilt.remote', 'quilt.contrib'],
-    install_requires=[
-        "Fabric >= 1.8.0",
-        "cuisine >= 0.6.5",
-    ],
+    install_requires=["Fabric >= 1.8.0", "cuisine >= 0.6.5"],
     zip_safe=False,
 )
