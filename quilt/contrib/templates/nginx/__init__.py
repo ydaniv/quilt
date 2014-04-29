8540dd90cc0a3362032cb718898f6e06ee83e73d
Paul Walsh <paulywalsh@gmail.com>
2014-04-29 16:32:34 +0300

    Fix nginx declaration of $host
    So that python does not perform string replacement on it.

--- quilt/contrib/templates/nginx/__init__.py
+++ quilt/contrib/templates/nginx/__init__.py
@@ -86,7 +86,7 @@ http {
             proxy_set_header        X-Forwarded-Proto $scheme;
             proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
             proxy_set_header        X-Real-IP $remote_addr;
-            proxy_set_header        Host $host;
+            proxy_set_header        Host $$host;
             proxy_redirect          off;
             proxy_pass              http://${project_name};
         }
